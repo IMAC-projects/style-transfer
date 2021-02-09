b3d9eea9f04fc180cd0cf6dd3848da44fbfc68c8
LafLaurine <laurine.lafontaine@outlook.fr>
2021-02-09 12:23:05 +0100

    [Add] - Surfaces distance

---
+++ src/surfaces_distance.py
@@ -0,0 +1,23 @@
+import pyvista as pv
+import numpy as np
+from scipy.spatial import KDTree
+
+p = pv.Plotter()
+obj1 = pv.read("../output/final_model.obj")
+obj2 = pv.read("../assets/meshes/01t.obj")
+p.add_mesh(obj1, smooth_shading=True)
+p.add_mesh(obj2, smooth_shading=True)
+p.show_grid()
+p.show()
+
+tree = KDTree(obj2.points)
+d, idx = tree.query(obj1.points)
+obj1["distances"] = d
+
+print("Distance :")
+print(np.mean(d))
+
+p = pv.Plotter()
+p.add_mesh(obj1, scalars="distances", smooth_shading=True)
+p.add_mesh(obj2, color=True, opacity=0.75, smooth_shading=True)
+p.show()
